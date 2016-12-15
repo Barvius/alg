15aac30c41a7aa4d86ac81ae9b8032945b06a2d6
barvius <barabanshikov.oleg@yandex.ru>
2016-12-16 01:37:40 +0300

    update lb10
    final version

--- lb10.cpp
+++ lb10.cpp
@@ -12,40 +12,35 @@
 #include <ctype.h>
 void main(){
 	setlocale(0,"rus");
-	FILE *uk;
-	char a[100];
-	char *ach ;
-	int i = 0,p=0, str_count=0, vs_vt_count=0, vs_vs_count=0, s_max=0, s_max_addr=0;
-
-	if (!(uk = fopen("text.txt", "r"))) {
+	FILE *f_in, *f_out;
+	char *ykaz, a[100], b[100], znak[] = { '.', ',', ':' };
+	int i, p, str_count, vs_vt_count, vs_vs_count, s_max, s_max_addr;
+	i = p = str_count = vs_vt_count = vs_vs_count = s_max = s_max_addr = 0;
+	if (!(f_in = fopen("text.txt", "r"))) {
 		printf("Файл не найден....\nСоздаю...\t[ok]\n");
-		uk = fopen("text.txt", "w");
-		fputs("Пример использования функции fopen ", uk); // записать строку в файл
-		fclose(uk);
-	}	
-	else
+		f_in = fopen("text.txt", "w");
+		fputs("Пример использования функции fopen ", f_in);
+		fclose(f_in);
+	} else {
 		printf("Файл найден\t[ok]\n");
+	}
 	printf("-----------------------------------------------\n");
-	while (fgets(a, 100, uk)) {
+	while (fgets(a, 100, f_in)) {
 		printf(a);
 	}
 	printf("\n-----------------------------------------------\n");
-	fseek(uk, 0L, SEEK_SET);
-	while (fgets(a, 100, uk)) {
-		//printf(a);
-		ach = &a[0];
+	fseek(f_in, 0L, SEEK_SET);
+	while (fgets(a, 100, f_in)) {
+		ykaz = &a[0];
 		do {
-			ach = strchr(ach + p, 'а');
-			//ach = strchr(ach + p, 'а');
+			ykaz = strchr(ykaz + p, 'а');
 			p++;
-			if (ach == NULL) {
-				//printf("Символ в строке не найден\n");
-			} else {
-				printf("Символ \"a\" в строке %d на позиции # %d\n", str_count +1, ach - a + 1);
+			if (ykaz){
+				printf("Символ \"a\" в строке %d на позиции # %d\n", str_count +1, ykaz - a + 1);
 				vs_vt_count++;
 				vs_vs_count++;
 			}
-		} while (ach);
+		} while (ykaz);
 		p = 0;
 		if (vs_vs_count > s_max) {
 			s_max = vs_vs_count;
@@ -56,5 +51,37 @@ void main(){
 	}
 	printf("Всего символов \"a\" в тексте %d\n", vs_vt_count);
 	printf("макс в строке %d\n", s_max_addr+1);
+	fseek(f_in, 0L, SEEK_SET);
+	i = -1;
+	do {
+		fgets(a, 100, f_in);
+		i++;
+	} while (i < s_max_addr);
+	for ( i = 0; i < strlen(znak); i++){
+		ykaz = &a[0];
+		do {
+			ykaz = strchr(ykaz + p, znak[i]);
+			p++;
+			if (ykaz) {
+				*ykaz = '*';
+			}
+		} while (ykaz);
+		p = 0;
+	}
+	printf(a);
+	f_out = fopen("out.txt", "w+");
+	str_count = 0;
+	fseek(f_in, 0L, SEEK_SET);
+	while (fgets(b, 100, f_in)) {
+		if (str_count == s_max_addr) {
+			fputs(a, f_out);
+			str_count++;
+			continue;
+		}
+		fputs(b, f_out);
+		str_count++;
+	}
+	fclose(f_out);
+	fclose(f_in);
 	_getch();
 }
