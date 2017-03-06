59fba1f5a1a8d7b18f3d5bf33aae39058f9c2536
barvius <barabanshikov.oleg@yandex.ru>
2017-03-06 20:48:39 +0300

    update

---
+++ lb13.cpp
@@ -0,0 +1,56 @@
+﻿#include "stdafx.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "conio.h"
+#include "locale.h"
+
+void FillRand(int *arr, int n) {
+	for (int i = 0; i < n; i++){
+		arr[i] = rand() % 20;
+	}
+}
+
+void PosMove(int *arr, int n) {
+	int i = 0, j = n - 1, l;
+	while (i < j) {
+		while (arr[i] % 2)i++;
+		while (arr[j] % 2)j--;
+		if (!(arr[i] % 2) && !(arr[j] % 2)) {
+			l = arr[i];
+			arr[i] = arr[j];
+			arr[j] = l;
+			i++, j--;
+		}
+	}
+}
+
+void PrintArr(int *arr, int n) {
+	for (int i = 0; i < n; i++) {
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+}
+
+void main(){
+	setlocale(0, "RUS");
+	int arr_1[10], n_1 = 10;
+	printf("Первый массив\n");
+	FillRand(arr_1,n_1);
+	PrintArr(arr_1, n_1);
+	PosMove(arr_1, n_1);
+	PrintArr(arr_1, n_1);
+	int arr_2[20], n_2 = 20;
+	printf("Второй массив\n");
+	FillRand(arr_2, n_2);
+	PrintArr(arr_2, n_2);
+	PosMove(arr_2, n_2);
+	PrintArr(arr_2, n_2);
+	int arr_3[5], n_3 = 5;
+	printf("Третий массив\n");
+	FillRand(arr_3, n_3);
+	PrintArr(arr_3, n_3);
+	PosMove(arr_3, n_3);
+	PrintArr(arr_3, n_3);
+	_getch();
+}
+
---
+++ lb14.cpp
@@ -0,0 +1,83 @@
+﻿#include "stdafx.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "conio.h"
+#include "locale.h"
+
+void FillRand(int *arr, int n, int m) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			*(arr+i*n+j) = rand() % 20;
+		}
+	}
+}
+
+void PrintArr(int *arr, int n, int m) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			printf(" %-2d ", *(arr + i*n+j));
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
+
+float SrAr(int *arr, int n, int m){
+	float S = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			S += *(arr + i*n + j);
+		}
+	}
+	return S / (m*n);
+}
+
+void SrArP(int *arr, int n, int m, float *S) {
+	*S = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			*S += *(arr + i*n + j);
+		}
+	}
+	*S /= (m*n);
+}
+
+float Percent(int *arr, int n, int m, float S) {
+	float count = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (*(arr + i*n + j) > S){
+				count++;
+			}
+		}
+	}
+	return count * 100 / (m*n);
+}
+
+void PercentP(int *arr, int n, int m, float S, float *P) {
+	float count = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (*(arr + i*n + j) > S) {
+				count++;
+			}
+		}
+	}
+	*P = count * 100 / (m*n);
+}
+
+void main() {
+	setlocale(0, "RUS");
+	int arr_1[10][10], n_1 = 10, m_1 = 10;
+	FillRand(&arr_1[0][0], n_1, m_1);
+	PrintArr(&arr_1[0][0], n_1, m_1);
+	printf("Ср ар. - %.1f\n", SrAr(&arr_1[0][0], n_1, m_1));
+	printf("Больше чем ср.ар - %.1f%%\n", Percent(&arr_1[0][0], n_1, m_1, SrAr(&arr_1[0][0], n_1, m_1)));
+	float S,P;
+	SrArP(&arr_1[0][0], n_1, m_1, &S);
+	PercentP(&arr_1[0][0], n_1, m_1, S, &P);
+	printf("Ср ар. - %.1f\n", S);
+	printf("Больше чем ср.ар - %.1f%%\n", P);
+	_getch();
+}
+
